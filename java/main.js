0c69f51ca92b84be21174e729ea469449d99cfed
OLEisBOSS <[ole.p.pawlowski@gmail.com]>
2023-04-23 21:55:50 +0200

    About-me Section Finished

--- java/main.js
+++ java/main.js
@@ -12,3 +12,24 @@ const enhance = id => {
 };
 
 enhance('span-2')
+
+const ole = document.getElementById('clickable');
+const figure = document.getElementById('figure')
+
+ole.addEventListener('mouseover', (e) => {
+    var aRect = e.target.getBoundingClientRect();
+    var aX = aRect.left + 15;
+    var aY = aRect.top + window.pageYOffset;
+
+    setTimeout(() => {
+        figure.style.left = `${Math.round(aX)}px`;
+        figure.style.top = `${Math.round(aY -110)}px`;
+        figure.style.display = 'block'; 
+    }, 600); 
+});
+
+ole.addEventListener('mouseout', (e) => {
+    setTimeout(() => {
+        figure.style.display = 'none'
+    }, 600)
+})
