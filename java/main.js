279bbb7623d9d4209ace3841681cb871a7e3d3d5
OLEisBOSS <[ole.p.pawlowski@gmail.com]>
2023-04-30 15:02:19 +0200

    First Version of Website completed

--- java/main.js
+++ java/main.js
@@ -1,3 +1,13 @@
+// header
+
+document.getElementsByClassName('toggle-button')[0].addEventListener('click', () => {
+    document.getElementsByClassName('nav')[0].classList.toggle('active');
+    document.getElementsByClassName('header1')[0].classList.toggle('active');
+    document.getElementsByClassName('toggle-button')[0].classList.toggle('active')});
+
+
+
+
 const enhance = id => {
      const element = document.getElementById(id), text = element.innerText.split("");
 
@@ -12,7 +22,7 @@ const enhance = id => {
 };
 
 enhance('span-2')
-
+/*
 const ole = document.getElementById('clickable');
 const figure = document.getElementById('figure')
 
@@ -33,3 +43,35 @@ ole.addEventListener('mouseout', (e) => {
         figure.style.display = 'none'
     }, 600)
 })
+
+*/
+
+//send Mail
+
+function sendMail(event) {
+    event.preventDefault(); // Prevent form submission and page reload
+
+    // Get form data
+    var params = {
+        name: document.getElementById('name').value,
+        number: document.getElementById('number').value,
+        email: document.getElementById('email').value,
+        task: document.getElementById('task').value,
+        message: document.getElementById('message').value,
+    };
+
+    // Send email using EmailJS
+    const serviceID = 'service_pprh0cp';
+    const templateID = 'template_455ckml';
+    emailjs.send(serviceID, templateID, params)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Your message has been sent successfully!');
+        }, function(error) {
+            console.log('FAILED...', error);
+            alert('There was an error sending your message. Please try again later.');
+        });
+}
+
+// Add event listener to form submit event
+document.querySelector('form').addEventListener('submit', sendMail);
